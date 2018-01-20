1039cf5567b0c6f4d533030df341c3cc88ea2b1f
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-20 05:14:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const app = "I don't do much."
 /*Is something wrong with this lab? I don't see anything except the above line which says:
 const app = "I don't do much"
-Where is the kitten stuff? Is everything culled from the "learn test"?
+Where is the kitten stuff? Is everything culled from the "learn test"? 
 */
 
 var kittens = ["Milo", "Otis", "Garfield"]
+
+destructivelyAppendKitten(name); {
+  return [...kittens, name]
+}
 
